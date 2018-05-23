2ea82d60a5c1b51c5d1e70ec5ba92b0be503953f
UncleJoe <zhouxiang.huang@gmail.com>
2018-05-24 00:46:59 +0800

    collection page

---
+++ pages/collections/collections.js
@@ -0,0 +1,24 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('ton.open-type.getUserInfo'),
+    categories: [],
+    newCategory: ''
+  },
+  //事件处理函数
+  bindViewTap: function () {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+   
+  },
+
+})
